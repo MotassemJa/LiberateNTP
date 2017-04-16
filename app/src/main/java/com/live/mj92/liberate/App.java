17507457940fc5ccf80fd30ecbb11bb12ad4eea9
MJ92 <m.j.92@live.com>
2017-04-16 14:38:12 +0300

    Beacons Monitoring

--- app/src/main/java/com/live/mj92/liberate/App.java
+++ app/src/main/java/com/live/mj92/liberate/App.java
@@ -2,8 +2,12 @@ package com.live.mj92.liberate;
 
 import android.app.Application;
 
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.service.BeaconManager;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 /**
  * Created by MJ92 on 4/16/2017.
@@ -11,12 +15,25 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class App extends Application {
 
+    private static BeaconManager mBeaconManager;
+
+    public static BeaconManager getmBeaconManager() {
+        return mBeaconManager;
+    }
+
+    public static void setmBeaconManager(BeaconManager mBeaconManager) {
+        App.mBeaconManager = mBeaconManager;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mBeaconManager = new BeaconManager(getApplicationContext());
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference();
 
     }
+
 }
--- app/src/main/java/com/live/mj92/liberate/MainActivity.java
+++ app/src/main/java/com/live/mj92/liberate/MainActivity.java
@@ -5,18 +5,28 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
 import com.live.mj92.liberate.fragments.DashboardFragment;
 import com.live.mj92.liberate.fragments.HomeFragment;
 import com.live.mj92.liberate.fragments.NotificationsFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private List<BeaconRegion> mRegions = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -28,17 +38,14 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mFragmentTransaction.replace(R.id.content, new HomeFragment());
-//                    mFragmentTransaction.addToBackStack("Home");
                     flag = true;
                     break;
                 case R.id.navigation_dashboard:
                     mFragmentTransaction.replace(R.id.content, new DashboardFragment());
-//                    mFragmentTransaction.addToBackStack("Dashboard");
                     flag = true;
                     break;
                 case R.id.navigation_notifications:
                     mFragmentTransaction.replace(R.id.content, new NotificationsFragment());
-//                    mFragmentTransaction.addToBackStack("Notification");
                     flag = true;
                     break;
             }
@@ -52,7 +59,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        fillBeacons();
+
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        App.getmBeaconManager().connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+//                App.getmBeaconManager().startRanging(mRegions.get(0));
+                App.getmBeaconManager().startMonitoring(mRegions.get(0));
+            }
+        });
     }
 
     @Override
@@ -64,6 +81,44 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_dashboard);
+
+//        App.getmBeaconManager().setRangingListener(new BeaconManager.BeaconRangingListener() {
+//            @Override
+//            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<Beacon> list) {
+//                if (!list.isEmpty()) {
+//                    Toast.makeText(getApplicationContext(), "Beacon Found", Toast.LENGTH_LONG).show();
+//                    Log.i("BeaconsAround", list.size() + " Beacons found");
+//                }
+//            }
+//        });
+
+        App.getmBeaconManager().setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
+            @Override
+            public void onEnteredRegion(BeaconRegion beaconRegion, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter", Toast.LENGTH_SHORT).show();
+                    Log.i("BeaconsAround", list.size() + " Beacons found - Enter");
+                }
+            }
+
+            @Override
+            public void onExitedRegion(BeaconRegion beaconRegion) {
+//                if (beaconRegion != null) {
+                    Toast.makeText(getApplicationContext(), "Exit", Toast.LENGTH_SHORT).show();
+                    Log.i("BeaconsAround", "Exit");
+//                }
+            }
+        });
+
     }
 
+    private void fillBeacons() {
+        mRegions.add(new BeaconRegion("RR", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null));
+    }
+
+    @Override
+    protected void onPause() {
+        App.getmBeaconManager().stopRanging(mRegions.get(0));
+        super.onPause();
+    }
 }
